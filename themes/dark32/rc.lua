31cd3ed0b974b153b520476086554c76a1c24abb
assisken <assisken@gmail.com>
2018-06-18 23:01:25 +0300

    The begining of theme switching. \o/

---
+++ themes/dark32/rc.lua
@@ -0,0 +1,14 @@
+local gears = require("gears")
+local awful = require("awful")
+--require("awful.autofocus")
+-- Виджеты и слои
+local wibox = require("wibox")
+-- Темы
+local beautiful = require("beautiful")
+-- Оповещение
+local naughty = require("naughty")
+local hotkeys_popup = require("awful.hotkeys_popup").widget
+-- Дополнительные возможности
+local lain = require("lain")
+
+require("lib.globalkey")
